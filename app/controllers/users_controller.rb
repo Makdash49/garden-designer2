d98236ad6acae4db5ac19985c77a75fa4e0d29c0
Mark McQuillen <markdmcq@hotmail.com>
2015-08-08 17:33:54 -0700

    show users and allow new ones to be added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+
   def new
-  	@message = "Hello World!"
+  	@users = User.all
+  	@user = User.new
   end
+
+  def create
+  	@user = User.create(user_params)
+  	@users = User.all 
+  	render 'new'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
+
 end
